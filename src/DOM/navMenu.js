0b2e6a1d2a3cb1df4a9cbbc177e67318fdc69cea
dwhale2 <devinwaynehale@gmail.com>
2023-06-01 10:56:10 -0500

    navMenu appearance working

--- src/DOM/navMenu.js
+++ src/DOM/navMenu.js
@@ -1,18 +1,44 @@
+import {Project} from '../LOGIC/projects.js';
+
 const navRender = () => {
     const navIcon = document.getElementById('navIcon');
 
     //Menu BKG
     let sideMenu = document.createElement('div');
     sideMenu.classList = 'sideMenu';
+    sideMenu.id = 'sideMenu';
+
+    let navHeader = document.createElement('div');
+    navHeader.classList = 'sideHeader';
+    
+        let navHText = document.createElement('p');
+        navHText.id = 'navHText';
+        navHText.innerHTML = 'Projects'
+        navHeader.appendChild(navHText);
+
+        let navBack = document.createElement('i');
+        navBack.classList = 'fa-solid fa-x navBack';
+        navBack.id = 'navBack'
+        navHeader.appendChild(navBack);
+
+    sideMenu.appendChild(navHeader);
 
 
+    document.getElementById('content').appendChild(sideMenu);
 
-    navIcon.appendChild(sideMenu);
-
-    navIcon.addEventListener('click', a => {
+    navIcon.addEventListener('click', () => {
         sideMenu.style.width = "275px";
         sideMenu.style.boxShadow = '0px 0px 5px black'
     })
+
+    navBack.addEventListener('click', () => {
+        sideMenu.style.width = "0px";
+        sideMenu.style.boxShadow = '0px 0px 0px'
+    })
 };
 
-export default navRender;
+const navItemRender = () => {
+    const pLibrary = Project.getPLib();
+}
+
+export {navRender, navItemRender};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.css';
 import renderHeader from './DOM/header.js';
 import {Project} from './LOGIC/projects.js';
-import navRender from './DOM/navMenu.js';
+import {navRender, navItemRender} from './DOM/navMenu.js';
 
 renderHeader();
 navRender();
+navItemRender();
